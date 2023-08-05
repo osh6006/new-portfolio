f9bbad34b07e7b66e27c7495fe3485634702a8ef
Hwang Seok O <ohs6006@gmail.com>
2023-08-05 21:01:52 +0900

    JS - Home

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
-const appEl = document.getElementById("app");
+// Header에 페이지 아래로 스크롤 시 다크 스타일링 적용
+const headerEl = document.querySelector(".header");
+const headerHeight = headerEl.getBoundingClientRect().height;
 
-appEl.innerHTML = "App Setting Test";
+document.addEventListener("scroll", () => {
+  if (window.scrollY > headerHeight) {
+    headerEl.classList.add("header--dark");
+  } else {
+    headerEl.classList.remove("header--dark");
+  }
+});
+
+// 페이지를 아래로 스크롤 시 home의 투명도 조절
+const homeEl = document.querySelector(".home__box");
+const homeHeight = homeEl.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  homeEl.style.opacity = 1 - window.scrollY / homeHeight;
+});
