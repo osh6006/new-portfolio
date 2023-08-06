8a67ab14a91fd081d22927c495499a599f561e52
Hwang Seok O <ohs6006@gmail.com>
2023-08-06 20:11:28 +0900

    JS - Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,38 @@
 "use strict";
+
+const categoryEl = document.querySelector(".categories");
+const categoryEls = document.querySelectorAll(".category");
+
+const projectsEl = document.querySelector(".projects");
+const projectEls = document.querySelectorAll(".project");
+
+categoryEl.addEventListener("click", e => {
+  const filter = e.target.dataset.type;
+
+  if (!filter) {
+    return;
+  }
+
+  handleActiveSelection(e.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector(".category--selected");
+  active.classList.remove("category--selected");
+  target.classList.add("category--selected");
+}
+
+function filterProjects(filter) {
+  projectsEl.classList.add("anim-out");
+  setTimeout(() => {
+    projectEls.forEach(project => {
+      if (filter === "All" || filter === project.dataset.type) {
+        project.style.display = "block";
+      } else {
+        project.style.display = "none";
+      }
+    });
+    projectsEl.classList.remove("anim-out");
+  }, 250);
+}
